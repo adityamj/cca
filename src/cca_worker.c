c015ce0e012b8ca3ac99c4d1afb7ae3b3d20705d
Aditya Jain <aditya8892@aditya-jain.in>
2015-05-04 22:00:18 +0530

    Updated neighbour radius to 1 from 2. Added missing call to birth() from sim_birth. Many other fixes

--- src/cca_worker.c
+++ src/cca_worker.c
@@ -13,7 +13,7 @@
 /*	Age
 	Activity group
 	existing infection
-*/
+ */
 
 void sim_birth( struct ca_cell * cell){
 	double p;
@@ -22,7 +22,7 @@ void sim_birth( struct ca_cell * cell){
 		cell->activity_group = acg_oracle( MIN_AGE * 12);
 		cell->occupied = 1;
 		cell->age = MIN_AGE * 12;
-
+		birth();
 		p = cca_rng_get();
 		if ( p <= conf.asir[ cell->age-MIN_AGE ]){
 			cell->current_status = 1;	
@@ -36,13 +36,13 @@ void infected( struct ca_cell * cell){
 	double rand;
 	rand = cca_rng_get();
 	cell->age_of_infection++;
-/*	if( cell->age_of_infection == 1){
+	if( cell->age_of_infection <= 1){
 		if( chooseit( conf.i_to_h)){
 			new_hsil();
 			cell->current_status = 3;
 		}	
 	}
-	else {	*/
+	else {	
 		if( rand <= ( p = i_to_n_p( cell->age))){	
 			cell->current_status = 0;
 			cell->age_of_infection = 0;
@@ -52,7 +52,7 @@ void infected( struct ca_cell * cell){
 			cell->current_status = 2;
 			new_lsil();
 		}
-//	}
+	}
 }
 
 void hsil( struct ca_cell * cell){
@@ -62,8 +62,8 @@ void hsil( struct ca_cell * cell){
 	p = 0;
 	cell->age_of_infection++;
 	if( rand <= ( p = h_to_l_p( cell->age_of_infection))){ // recovery to lsil
-		if( cell->age_of_infection <= conf.h_to_l_time) 
-		hsil_to_lsil();
+		//if( cell->age_of_infection <= conf.h_to_l_time) 
+			hsil_to_lsil();
 		new_lsil();
 		cell->current_status = 2;
 	}
@@ -84,10 +84,10 @@ void lsil( struct ca_cell * cell){
 		new_hsil( p);
 		cell->current_status = 3;
 	}
-	else if( rand <= ( p += l_to_i_p(cell->age, cell->age_of_infection)) ){	// cancer
+	else if( rand <= ( p += l_to_i_p(cell->age, cell->age_of_infection)) ){
 		cell->current_status = 0;
 		if( cell->age_of_infection <= conf.l_to_n_time)	
-		lsil_to_normal();
+			lsil_to_normal();
 		cell->age_of_infection = 0;
 	}
 }
@@ -105,6 +105,7 @@ void cancer( struct ca_cell * cell){
 	if( choice){
 		death();
 		memset( cell, 0, sizeof(struct ca_cell));
+		cell->num_p = 0;
 	}
 
 }
@@ -132,7 +133,7 @@ void populate(struct ca_grid * t){
 			}
 			birth();
 			curr_cell->occupied = 1;
-			curr_cell->age =age * 12;
+			curr_cell->age =age * 12 + 11.9* cca_rng_get();
 			curr_cell->activity_group = acg_oracle(curr_cell->age);
 			rand = cca_rng_get();
 			if( curr_cell->current_status ){
@@ -147,26 +148,26 @@ void populate(struct ca_grid * t){
 	}
 }
 
-int should_infect( struct ca_grid *t, int x, int y, int z, double p){
+int should_infect( const struct ca_grid *t, int x, int y, int z, double p){
 	int xx, yy, zz;
 	int ineigh=0;
 	int ret = 0;
-	struct ca_cell * curr;
-	xx = x -2;
-	yy = y -2;
-	zz = z -2;
+	const struct ca_cell * curr;
+	xx = x -1;
+	yy = y -1;
+	zz = z -1;
 	if( xx < 0)
 		xx = 0 ;
 
-	for( ; (xx <= ( x+2)) && (xx < t->size); xx++){
-		yy = y -2;
+	for( ; (xx <= ( x+1)) && (xx < t->size); xx++){
+		yy = y -1;
 		if( yy < 0)
 			yy = 0;
-		for(; (yy <= (y+2)) && (yy < t->size) ; yy++){
-			zz = z -2;	
+		for(; (yy <= (y+1)) && (yy < t->size) ; yy++){
+			zz = z -1;	
 			if ( zz < 0)
 				zz = 0;
-			for( ;( zz <= (z+2)) &&( zz < t->size); zz++){
+			for( ;( zz <= (z+1)) &&( zz < t->size); zz++){
 				curr = translate( t->cells, xx,yy,zz, t->size);
 				if( xx == x && yy == y && zz == z)
 					continue;
@@ -184,13 +185,13 @@ int should_infect( struct ca_grid *t, int x, int y, int z, double p){
 	return ret;
 }
 
-int get_num_ineigh( struct ca_grid *t, int x, int y, int z){
+int get_num_ineigh( const struct ca_grid *t, int x, int y, int z){
 	int xx, yy, zz;
 	int neigh=0;
-	struct ca_cell * curr;
-	xx = x -2;
-	yy = y -2;
-	zz = z -2;
+	const struct ca_cell * curr;
+	xx = x -1;
+	yy = y -1;
+	zz = z -1;
 	if( xx < 0)
 		xx = 0 ;
 
@@ -200,33 +201,33 @@ int get_num_ineigh( struct ca_grid *t, int x, int y, int z){
 	if ( zz < 0)
 		zz = 0;
 
-	for( ; (xx <= ( x+2)) && (xx < t->size); xx++){
-		yy = y -2;
+	for( ; (xx <= ( x+1)) && (xx < t->size); xx++){
+		yy = y -1;
 		if( yy < 0)
 			yy = 0;
-		for(; (yy <= (y+2)) && (yy < t->size) ; yy++){
-			zz = z -2;	
+		for(; (yy <= (y+1)) && (yy < t->size) ; yy++){
+			zz = z -1;	
 			if ( zz < 0)
 				zz = 0;
-			for( ;( zz <= (z+2)) &&( zz < t->size); zz++){
+			for( ;( zz <= (z+1)) &&( zz < t->size); zz++){
 				curr = translate( t->cells, xx,yy,zz, t->size);
 				if( xx == x && yy == y && zz == z)
 					continue;
 				if( curr->occupied)
 					if( curr->current_status % 5 !=0)
-					neigh++;
+						neigh++;
 			}
 		}
 	}
 	return neigh;
 }
-int get_num_neigh( struct ca_grid *t, int x, int y, int z){
+int get_num_neigh( const struct ca_grid *t, int x, int y, int z){
 	int xx, yy, zz;
 	int neigh=0;
 	struct ca_cell * curr;
-	xx = x -2;
-	yy = y -2;
-	zz = z -2;
+	xx = x -1;
+	yy = y -1;
+	zz = z -1;
 	if( xx < 0)
 		xx = 0 ;
 
@@ -236,15 +237,15 @@ int get_num_neigh( struct ca_grid *t, int x, int y, int z){
 	if ( zz < 0)
 		zz = 0;
 
-	for( ; (xx <= ( x+2)) && (xx < t->size); xx++){
-		yy = y -2;
+	for( ; (xx <= ( x+1)) && (xx < t->size); xx++){
+		yy = y -1;
 		if( yy < 0)
 			yy = 0;
-		for(; (yy <= (y+2)) && (yy < t->size) ; yy++){
-			zz = z -2;	
+		for(; (yy <= (y+1)) && (yy < t->size) ; yy++){
+			zz = z -1;	
 			if ( zz < 0)
 				zz = 0;
-			for( ;( zz <= (z+2)) &&( zz < t->size); zz++){
+			for( ;( zz <= (z+1)) &&( zz < t->size); zz++){
 				curr = translate( t->cells, xx,yy,zz, t->size);
 				if( xx == x && yy == y && zz == z)
 					continue;
@@ -272,14 +273,16 @@ void simulate ( struct ca_grid * t, struct ca_grid * t1){
 				t1_cell = translate ( t1->cells, x,y,z, t->size);
 				memcpy ( t1_cell, t_cell, sizeof(struct ca_cell));	// Initialize our new cell.
 				if( t1_cell->occupied ){
-					if(should_die( t1_cell)){
-						memset( t1_cell, 0, sizeof(struct ca_cell));
-						death();	
-						continue;
-					}
 					t1_cell->age++;
 					if ( t1_cell->age > MAX_AGE *12){
+						death();
 						memset( t1_cell, 0, sizeof(struct ca_cell));
+						continue;
+					}
+					if(should_die( t1_cell)){
+						memset( t1_cell, 0, sizeof(struct ca_cell));
+						t1_cell->num_p = 0;
+						death();	
 						continue;
 					}
 					if ( is_ag_transition( t1_cell->age)){
@@ -289,8 +292,8 @@ void simulate ( struct ca_grid * t, struct ca_grid * t1){
 						case 0:{
 							// S
 							int g;
-								g = tellag( t1_cell->age);
-								if( g < 0)
+							g = tellag( t1_cell->age);
+							if( g < 0)
 								exit(507);	
 							t1_cell->num_p += (double) num_part(t_cell->age, t_cell->activity_group)/(double) 12 ;
 							num_p = t1_cell->num_p;
@@ -298,9 +301,9 @@ void simulate ( struct ca_grid * t, struct ca_grid * t1){
 							if ( ! num_p)
 								continue;
 							num_neigh = get_num_neigh( t, x,y,z);
-//							ret = should_infect( t,x,y,z, (double) num_p/ num_neigh);
-							ret = chooseit( get_num_ineigh( t,x,y,z)/(double)get_num_neigh(t,x,y,z) *
-								conf.activity[g][t1_cell->activity_group] / 12 * conf.p_transmission) ;
+							ret = should_infect( t,x,y,z, (double) num_p/ num_neigh);
+/*							ret = chooseit( get_num_ineigh( t,x,y,z)/(double)get_num_neigh(t,x,y,z) *
+									conf.activity[g][t1_cell->activity_group] / 12 * conf.p_transmission) ;				*/
 							if(ret){
 								// Infected
 								t1_cell->ever_infected = 1;
@@ -333,7 +336,7 @@ void simulate ( struct ca_grid * t, struct ca_grid * t1){
 					}
 				}
 				else{
-				// add a new individual to the simulation ?birth()
+					// add a new individual to the simulation ?birth()
 					sim_birth( t1_cell);
 				}
 			}	
@@ -387,6 +390,7 @@ int do_reporting( struct ca_grid * t, int count, int pid){
 		fprintf(fp, "%d,%d,hsil,%d\n",count, c, hsil[c]);
 		fprintf(fp, "%d,%d,cancer,%d\n",count, c, cancer[c]);
 	}
+	fflush ( fp);
 	return 0;
 }
 
@@ -396,18 +400,19 @@ int worker_fork(int pindex){
 	int ret;
 	struct ca_grid  t;
 	struct ca_grid  t1;
-	struct ca_grid  swap;
+	struct ca_cell  *swap;
 	int memsize;
 	memsize = conf.size*conf.size*conf.size;
 	t.size = conf.size;
 	t1.size = conf.size;
-	
-        if((ret = cca_rng_init()))
+
+	if((ret = cca_rng_init()))
 		return (ret+30);	
-	
+
 	if ( ( t.cells = (struct ca_cell *) calloc( memsize, sizeof(struct ca_cell))) == NULL){
-		fprintf(stderr,"Unable to allocate memory. exiting. size: %d\nstruct size: %lu\ntotal mem req: %lu", memsize, sizeof(struct ca_cell), memsize*sizeof(struct ca_cell));
-			
+		fprintf(stderr,"Unable to allocate memory. exiting. size: %d\nstruct size: %lu\ntotal mem req: %lu",
+				memsize, sizeof(struct ca_cell), memsize*sizeof(struct ca_cell));
+
 		return (8);
 	}
 
@@ -415,27 +420,33 @@ int worker_fork(int pindex){
 		fprintf(stderr,"Unable to allocate memory. exiting.\n");
 		return (8);
 	}
-	do_reporting( &t, -1, pindex);
 	populate(&t);
-do_reporting( &t, -2, pindex);
-/* Now populated, rand init complete. start simulattion	*/
+	print_state_stats(stderr);
+	swap = t.cells;
+	for( count = 0 ; count < memsize ; count ++)
+		(swap + count)->num_p = 0;
+	/* Now populated, rand init complete. start simulattion	*/
 	for( count = 0; count <= conf.pass; count++){
 		fprintf(stderr, "fork:%d\tSIM_round:%d\n",pindex,count);
 		new_month();
 		if( count%12 == 0)
 			reset_yearly_counts(); // vaccination & screening
 		simulate( &t, &t1);
-		swap.cells = t.cells;
+		swap = t.cells;
 		t.cells = t1.cells;
-		t1.cells = swap.cells;
+		t1.cells = swap;
 		if( count%60 == 0){
-			    if( (ret = do_reporting( &t, count, pindex)) ){
+			if( (ret = do_reporting( &t, count, pindex)) ){
 				free(t.cells);
 				free(t1.cells);
 				return ( ret + 20);
-			    }
+			}
 		}
+		print_state_stats(stderr);
 	}
+	print_state_stats(stderr);
+	free(t.cells);
+	free(t1.cells);
 	fprintf(stderr, "simualtion completed\ncount:%d\n",count);
 	if( (ret = do_reporting( &t, count, pindex)) ){
 		free(t.cells);
--- src/include/transition_helper.h
+++ src/include/transition_helper.h
@@ -2,6 +2,7 @@
 #define __TRANSITION_HELPER_H__
 #include <cca_ca.h>
 #include <cca.h>
+#include <stdio.h>
 
 extern double birth_p();
 extern double c_to_d_p();
@@ -23,5 +24,6 @@ extern double l_to_h_p ( int age) ; //func
 extern double h_to_l_p( int age_of_infection);	//func
 extern double h_to_c_p(); //func
 extern void new_lsil();
+extern void print_state_stats( FILE * fp);
 extern void new_month();
 #endif
